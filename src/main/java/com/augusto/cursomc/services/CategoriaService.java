8188b213cec3af96fce8d3c4ddb4b847a3240cb8
Augusto Guimarães <guguinha7@gmail.com>
2019-04-27 08:30:33 -0300

    metodo DELETE Categoria implementado

--- src/main/java/com/augusto/cursomc/services/CategoriaService.java
+++ src/main/java/com/augusto/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.augusto.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.augusto.cursomc.domain.Categoria;
 import com.augusto.cursomc.repositories.CategoriaRepository;
+import com.augusto.cursomc.services.exception.DataIntegrityException;
 import com.augusto.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
