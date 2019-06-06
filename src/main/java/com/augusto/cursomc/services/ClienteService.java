68989b6f9581caa95bc7ccb784bbffaba9840638
Augusto Guimarães <guguinha7@gmail.com>
2019-06-06 12:00:42 -0300

    Ajustando e testando deleção de Clientes

--- src/main/java/com/augusto/cursomc/services/ClienteService.java
+++ src/main/java/com/augusto/cursomc/services/ClienteService.java
@@ -65,7 +65,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		}catch (DataIntegrityViolationException e){
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
