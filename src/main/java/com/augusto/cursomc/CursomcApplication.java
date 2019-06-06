2a8f18e1e079a75b1f05dce663cfd5b755d07bb2
Augusto Guimarães <guguinha7@gmail.com>
2019-06-05 23:39:08 -0300

    add Inserção de um novo cliente com POST

--- src/main/java/com/augusto/cursomc/CursomcApplication.java
+++ src/main/java/com/augusto/cursomc/CursomcApplication.java
@@ -86,16 +86,19 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Estado est1 = new Estado(null,"Minas Gerais");
 		Estado est2 = new Estado(null,"São Paulo");
+		Estado est3 = new Estado(null,"Paraíba");
 		
 		Cidade c1 = new Cidade(null,"Uberlândia",est1);
 		Cidade c2 = new Cidade(null,"São Paulo",est2);
 		Cidade c3 = new Cidade(null,"Campinas",est2);
+		Cidade c4 = new Cidade(null,"João Pessoa",est3);
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		est3.getCidades().addAll(Arrays.asList(c4));
 		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2,est3));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
--- src/main/java/com/augusto/cursomc/services/ClienteService.java
+++ src/main/java/com/augusto/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.augusto.cursomc.domain.Cidade;
 import com.augusto.cursomc.domain.Cliente;
+import com.augusto.cursomc.domain.Endereco;
+import com.augusto.cursomc.domain.enums.TipoCliente;
 import com.augusto.cursomc.dto.ClienteDTO;
+import com.augusto.cursomc.dto.ClienteNewDTO;
 import com.augusto.cursomc.repositories.ClienteRepository;
+import com.augusto.cursomc.repositories.EnderecoRepository;
 import com.augusto.cursomc.services.exception.DataIntegrityException;
 import com.augusto.cursomc.services.exception.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -35,6 +44,14 @@ public class ClienteService {
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -54,6 +71,22 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(),objDto.getCpfOuCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
