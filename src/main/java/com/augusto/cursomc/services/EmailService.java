8e9035101a8a68032a44313098debfe18c6a5270
Augusto Guimarães <guguinha7@gmail.com>
2020-05-01 23:28:21 -0300

    Email HTML

--- src/main/java/com/augusto/cursomc/services/EmailService.java
+++ src/main/java/com/augusto/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.augusto.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.augusto.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/augusto/cursomc/services/MockEmailService.java
+++ src/main/java/com/augusto/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.augusto.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,6 +15,13 @@ public class MockEmailService extends AbstractEmailService {
 		LOG.info("Simulando envio de email...");
 		LOG.info("{}", msg);
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML");
+		LOG.info("{}", msg);
+		LOG.info("Email enviado");	
 	}
 
 }
